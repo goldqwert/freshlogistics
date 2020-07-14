78e9ef4bc4d11abb16b51706ef0e726aca151544
Yury <goldyukol12@gmail.com>
2020-07-14 03:03:02 +0300

    Delete bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import "./App.css";
-import Header from "./components";
+import s from "./App.module.css";
+import { Route, withRouter, Switch, Redirect } from "react-router-dom";
+import { Home, Busses, About, Contacts } from "./pages";
+import { Header } from "./components";
 
 const App = () => {
   return (
-    <>
-      <Header />
-    </>
+    <Switch>
+      <div className={s.wrapper}>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/busses" component={Busses} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contacts" component={Contacts} />
+      </div>
+    </Switch>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 import Header from "./Header/Header.js";
 
-export default Header;
+export { Header };
